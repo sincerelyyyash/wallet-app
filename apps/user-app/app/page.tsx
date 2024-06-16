6892eedeaad80968e07a0bdf93b1253fa48ea83d
sincerelyyyash <yta0088@gmail.com>
2024-06-16 13:15:47 +0530

    Swiched to starter repo

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,12 @@
-
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
 export default function Page(): JSX.Element {
-  return <div className="text-2xl">
-    Hello
-  </div>
+  const session = useSession();
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
 }
