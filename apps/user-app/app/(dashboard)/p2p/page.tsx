af37d92031117e8c1a6a9253f261617de83a0075
sincerelyyyash <yta0088@gmail.com>
2024-07-02 23:22:47 +0530

    p2p page updated

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -1,13 +1,58 @@
 import { SendCard } from "../../../components/SendMoneyCard"
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { OnRampTransactions } from "../../../components/OnRampTransactions";
 
 
-function page() {
-  return (
-    <div className="w-full">
-        <SendCard/>
-
-    </div>
-  )
+async function getBalance() {
+  const session = await getServerSession(authOptions);
+  const balance = await prisma.balance.findFirst({
+    where: {
+      userId: Number(session?.user?.id)
+    }
+  });
+  return {
+    amount: balance?.amount || 0,
+    locked: balance?.locked || 0
+  }
 }
 
-export default page
+async function getOnRampTransactions() {
+  const session = await getServerSession(authOptions);
+  const txns = await prisma.onRampTransaction.findMany({
+    where: {
+      userId: Number(session?.user?.id)
+    }
+  });
+  return txns.map(t => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider
+  }))
+}
+export default async function() {
+  const balance = await getBalance();
+  const transactions = await getOnRampTransactions();
+
+  return (
+    <div className="w-screen p-4">
+      <div className="text-4xl text-[#6a51a6] mb-8 font-bold text-center">
+        Transfer
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="flex justify-center">
+          <SendCard />
+        </div>
+        <div>
+          <BalanceCard amount={balance.amount} locked={balance.locked} />
+          <div className="pt-4">
+            <OnRampTransactions transactions={transactions} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
