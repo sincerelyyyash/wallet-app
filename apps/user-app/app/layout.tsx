75176b3d7af3a6988bc7496247deb91c1248da2f
sincerelyyyash <yta0088@gmail.com>
2024-09-22 17:10:39 +0530

    providers added

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Providers } from "@/providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <Providers>      <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      </Providers>
     </html>
   );
 }
